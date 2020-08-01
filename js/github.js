acd8976807c54d7168ee89493fadb44a5b9df96a
Mauro M <hello@maurom.dev>
2020-08-01 16:33:01 +0100

    Refined criteria for valid repository

--- js/github.js
+++ js/github.js
@@ -11,8 +11,7 @@ request.open('GET', 'https://api.github.com/users/MM-coder/repos', true)
 request.onload = function () {
     var data = JSON.parse(this.response)
     data.forEach(i => {
-        if (((Math.ceil((Date.now() - Date.parse(i['updated_at'])) / (1000 * 60 * 60 * 24))) <= 7 || (i['stargazers_count'] >= 10 && i['fork'] == false)) && i['description'] != null) {
-            
+        if (((Math.ceil((Date.now() - Date.parse(i['updated_at'])) / (1000 * 60 * 60 * 24))) <= 7 || (i['stargazers_count'] >= 10 && i['fork'] == false)) && i['description'] != null &&  i['fork'] == false) {
             var html = `<div data-uk-scrollspy-class="uk-animation-slide-left-medium">
                             <div class="uk-card uk-card-body">
                                 <h3 class="uk-card-title">${i['name']}</h3>
